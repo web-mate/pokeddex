74bb886141866eca7ab014e50c7b50da77cace0a
=webmate <=>
2016-07-24 21:08:43 +0300

    Initial Commit

---
+++ pokeddex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeddex
+//
+//  Created by dimitris vlagas on 24/7/16.
+//  Copyright © 2016 webmate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
